e83bdce05312080816cfc2bc546bb9b2cf17f095
NareshSanjeev <naresh732003@gmail.com>
2025-12-10 08:37:08 +0530

    implemented completed strike,theme swiching and lucida - react icons add some more style related to the dxark theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,30 @@ import { useState } from "react";
 import "./App.css";
 import TodoAdd from "./components/TodoAdd";
 import TodoList from "./components/TodoList";
+import { MoonIcon } from "lucide-react";
+import { Sun } from "lucide-react";
 
 function App() {
-  const[todos,setTodos]=useState([])
+  const [todos, setTodos] = useState([]);
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
 
   return (
-    <>
+    <div className={`app ${theme}`}>
       <h1>Todo App</h1>
-      <TodoAdd todos={todos} setTodos={setTodos}/>
+      <button className="theme-btn" onClick={toggleTheme}>
+        {theme === "light" ? (
+          <MoonIcon/>
+        ) : (
+           <Sun/>
+        )}
+      </button>
+      <TodoAdd todos={todos} setTodos={setTodos} />
       <TodoList todos={todos} setTodos={setTodos} />
-    </>
+    </div>
   );
 }
 
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-
+import "../styles/todoitem.css";
 function TodoItem({ id, description, isCompleted, todos, setTodos }) {
-    const handleDelete=(id)=>{
-        // debugger
-        const newTodos= todos.filter((todo)=>todo.id!==id)
-        console.log("clicked",{setTodos});
-       setTodos(newTodos)
-    }
+  const handleDelete = (id) => {
+    // debugger
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    console.log("clicked", { setTodos });
+    setTodos(newTodos);
+  };
+   const handleCheckbox = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    );
+    setTodos(updatedTodos);
+  };
   return (
-    <tr>
+    <tr className={isCompleted ? "completed-item" : ""}>
       <td>{id}</td>
       <td>{description}</td>
       <td>
         <input
           type="checkbox"
           checked={isCompleted}
+          onChange={() => handleCheckbox(id)} 
           className={isCompleted ? "completed" : ""}
         />
       </td>
